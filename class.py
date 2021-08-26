46451e17cae06f1f7c8f2399cb6394f5affd02cb
Arpith samuel <samuel.arpith950@gmail.com>
2021-08-26 10:49:59 +0530

    py added

---
+++ class.py
@@ -0,0 +1,13 @@
+class cars:
+
+   def __init__(self,name,color,rollno):
+
+     self.name=benz
+     self.color=blue
+     self.rollno=10
+
+   def talk(self):
+      print(self.name,"-",self.rollno)
+
+b=cars()
+b.talk()
